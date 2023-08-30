ca5a15e2050e7c2c8fb04dcd9201cae0c8450390
Drdragon313 <muhammadahmad742@gmail.com>
2023-08-30 11:52:15 +0500

    git commit -a

---
+++ src/Features/todo/todoSlice.js
@@ -0,0 +1,49 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+const initialState = {
+  todos: [],
+};
+
+export const todoSlice = createSlice({
+  name: "todos  ",
+  initialState,
+  reducers: {
+    setInititalState: (state, action) => {
+        const listOfTodos = action.payload;
+        state.todos = listOfTodos;
+    },
+    addTodoSlice: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    editTodoSlice: (state, action) => {
+        const { id, newText } = action.payload;
+        const existingTodo = state.todos.find((todo) => todo.id === id);
+        if (existingTodo) {
+          existingTodo.text = newText;
+        }
+      },
+    toggleDoneSlice:(state, action)=>{
+        const { id  } = action.payload;
+        const existingTodo = state.todos.find((todo) => todo.id === id);
+        if (existingTodo) {
+          existingTodo.done = !existingTodo.done;
+        }
+
+
+        // const { id} = action.payload;
+        // const updatedTodos = state.todos.map((todo) =>
+        // todo.id === id ? { ...todo, done: !todo.done } : todo
+    //   );
+    //   updatedTodos.id=id
+    }
+  }
+});
+
+export const { addTodoSlice, removeTodo,editTodoSlice,toggleDoneSlice, setInititalState } = todoSlice.actions;
+export default todoSlice.reducer;
